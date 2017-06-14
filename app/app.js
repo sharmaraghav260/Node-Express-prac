d2e90ee6db172c686b231290d7fad2bc409b8b3a
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-14 17:32:13 -0600

    Basic routing

--- app/app.js
+++ app/app.js
@@ -9,6 +9,13 @@ app.set('port', process.env.PORT || 3000);
 var port = app.get('port');
 
 app.get('/', function(req, res) {
+  res.send(`
+    <h1> Hello Guys,</h1>
+    <p> Welcome to an application built with Node and Express.</p>
+  `);
+});
+
+app.get('/speakers', function(req, res) {
   var info = "";
   dataFile.speakers.forEach(function(item) {
     info += `
@@ -21,6 +28,15 @@ app.get('/', function(req, res) {
   res.send(`
     <h1> Express Works</h1>
     ${info}
+  `);
+});
+
+app.get('/speakers/:speakerid', function(req, res) {
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+    <h1>${speaker.title}</h1>
+    <h2>with ${speaker.name}</h2>
+    <p>${speaker.summary}</p>
   `);
 });
 
