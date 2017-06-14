e36943f0ed111bfe260b72e808866749bb60fffe
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-14 16:47:09 -0600

    http req with express

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
-var http = require('http');
+//var http = require('http');
+var express = require('express');
+var app = express();
 
-var myServer = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    "cotent-Type": "text/gtml"
-  });
-  res.write('<h1>Hello Guys</h1>');
-  res.end();
+app.get('/', function(req, res) {
+  res.send('<h1> Express Works</h1>');
 });
 
-myServer.listen(3000);
-console.log("Go to http://localhost:3000");
+var server = app.listen(3000, function() {
+  console.log('listening on port 3000');
+});
+
+// var myServer = http.createServer(function(req, res) {
+//   res.writeHead(200, {
+//     "cotent-Type": "text/gtml"
+//   });
+//   res.write('<h1>Hello Guys</h1>');
+//   res.end();
+// });
+//
+// myServer.listen(3000);
+// console.log("Go to http://localhost:3000");
