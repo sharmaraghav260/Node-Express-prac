35c9a90669c3051dbfdb2c96ef5e6f1ea81a9012
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-06-14 22:32:49 -0600

    PROJECT DONE

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
+var io = require('socket.io')();
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData', dataFile);
@@ -20,10 +21,17 @@ app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 app.use(require('./routes/feedback'));
 app.use(require('./routes/api'));
-
+app.use(require('./routes/chat'));
 
 var server = app.listen(port, function() {
   console.log('listening on port ' + port);
+});
+
+io.attach(server);
+io.on('connection', function(socket) {
+  socket.on('postMessage', function(data) {
+    io.emit('updateMessages', data);
+  });
 });
 
 reload(server, app);
